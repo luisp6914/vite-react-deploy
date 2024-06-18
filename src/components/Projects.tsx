13250d9a7db1a36caa711f8491a29c0c3db9ebba
luisp6914 <luisp6914@gmail.com>
2024-06-17 17:54:25 -0700

    Added In Progress Ribbon

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -30,12 +30,53 @@ function Projects(){
 
 
     const projects = [
-        {class: 'covidProject', icon: faHospital, name: 'Covid Project', discription: 'Developed an online system to help a hypothetical local hospital for its COVID vaccination effort. The system will keep track of both patient and vaccine information for the hospital', path: '/covid-project'},
-        {class: 'pcPartPicker', icon: faComputer, name: 'PC Part Picker', discription: 'Developed an interface where users can select different PC parts from a given list. The price for the selected PC components are then displayed at checkout.', path: '/pc-part-picker'},
-        {class: 'teslaStockProject', icon: faMoneyBillTrendUp, name: 'Tesla Stocks Project', discription: 'Developed a program that reads and processes historic stock data for the Tesla Company. The data is extracted from a .csv file and then processed to display the information.', path: '/tesla-stocks-project'},
-        {class: 'linkedList', icon: faVectorSquare, name: '2D Liked List Project', discription: 'Developed a generic program that accepts an array list of information and turns it into 2D Linked List data structure.', path: '/linked-list'},
-        {class: 'dijiKeyAPI', icon: faCloud, name: 'DijiKey API', discription: 'Developed an Express.js Server Application. The Server Application interacts with DijiKey API and stores data on a Mongo Atlas Cloud Database.', path: '/digikey-api'}
-    ]
+      {
+        class: "covidProject",
+        icon: faHospital,
+        name: "Covid Project",
+        discription:
+          "Developed an online system to help a hypothetical local hospital for its COVID vaccination effort. The system will keep track of both patient and vaccine information for the hospital",
+        path: "/covid-project",
+        finished: false,
+      },
+      {
+        class: "pcPartPicker",
+        icon: faComputer,
+        name: "PC Part Picker",
+        discription:
+          "Developed an interface where users can select different PC parts from a given list. The price for the selected PC components are then displayed at checkout.",
+        path: "/pc-part-picker",
+        finished: false,
+      },
+      {
+        class: "teslaStockProject",
+        icon: faMoneyBillTrendUp,
+        name: "Tesla Stocks Project",
+        discription:
+          "Developed a program that reads and processes historic stock data for the Tesla Company. The data is extracted from a .csv file and then processed to display the information.",
+        path: "/tesla-stocks-project",
+        finished: false,
+      },
+      {
+        class: "linkedList",
+        icon: faVectorSquare,
+        name: "2D Liked List Project",
+        discription:
+          "Developed a generic program that accepts an array list of information and turns it into 2D Linked List data structure.",
+        path: "/linked-list",
+        finished: false,
+      },
+      {
+        class: "dijiKeyAPI",
+        icon: faCloud,
+        name: "DijiKey API",
+        discription:
+          "Developed an Express.js Server Application. The Server Application interacts with DijiKey API and stores data on a Mongo Atlas Cloud Database.",
+        path: "/digikey-api",
+        finished: false,
+      },
+    ];
+  
 
     return(
         <div className="container">
@@ -43,13 +84,16 @@ function Projects(){
             <div className="projects row">
                 {projects.map((project, index) => (
                     <div key={index} className="card project col-md-4 fade-in" style={{width: '400px', height: '350px'}}>
+                        {!project.finished && (
+                          <div className="ribbon"><span>In Progress</span></div>
+                        )}
                         <div className={`icon-container ${project.class}`}>
                             <FontAwesomeIcon icon={project.icon} className='fa-icon'></FontAwesomeIcon>
                         </div>
                         <div className="card-body">
                             <h3 className="card-title">{project.name}</h3>
                             <p className="card-text">{project.discription}</p>
-                            <Link className="btn btn-primary rounded-pill try-it" to={project.path}>Try It</Link>
+                            <Link className={`btn btn-primary rounded-pill try-it ${!project.finished ? 'disabled' : ''}`} to={project.path}>Try It</Link>
                         </div>
                     </div>
                 ))}
