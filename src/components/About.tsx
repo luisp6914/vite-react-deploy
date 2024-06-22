4912cfbd54ee37914f8f836377ea012d66d0e324
luisp6914 <luisp6914@gmail.com>
2024-06-22 12:01:23 -0700

    Updated About Me text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,9 +18,10 @@ function About() {
             <h3>About Me</h3>
             <div>
                 <p>
-                  I'm a recent graduate of the Class of 2024 with a 
-                  passion for both front-end and back-end development. Throughout my educational career, 
-                  I have developed my abilities and I'm excited to bring my knowledge and enthusiasm to the professional world.
+                  I have a strong passion for both front-end and back-end development, which drives me to continuously enhance my skills in these
+                  areas. In a previous role as an AWS team leader, we improved our workflow by implementing AWS Secrets Manager, which bolstered
+                  our security measures. In another role, I developed webpages using HTML, CSS, and JavaScript. Additionally, I assisted with daily
+                  tasks by updating product information for an e-commerce company, ensuring the website's content remained accurate and up-to-date
                 </p>
                 <p>
                   When I'm not coding, you'll often find me immersed in video games and embracing 
@@ -28,7 +29,7 @@ function About() {
                   whether it's playing a pickup game with friends or watching the latest Lakers Game.
                 </p>
                 <p>
-                  Graduating in 2024 marks the beginning of an exciting chapter in my life, and I can't 
+                  Graduating from college marked the beginning of an exciting chapter in my life, and I can't 
                   wait to embark on my career. I'm eager to contribute to innovative projects, collaborate 
                   with talented individuals, and continue learning and growing in this ever-evolving field. 
                 </p>
